d0f164f321704116f508969316c55b6592df61e7
Sergey <vashenko49@gmail.com>
2019-08-16 22:47:15 +0300

    finish hw2

--- homework2/js/main.js
+++ homework2/js/main.js
@@ -39,16 +39,9 @@ const toppings = [
 class Hamburger{
     constructor(size, stuffing) {
         this.size = size;
-        this.stuffing = stuffing
+        this.stuffing = stuffing;
+        this._topping = [];
     }
-
-    static SIZE_SMALL = "SIZE_SMALL";
-    static SIZE_LARGE = "SIZE_LARGE";
-    static STUFFING_CHEESE = 'STUFFING_CHEESE';
-    static STUFFING_SALAD = "STUFFING_SALAD";
-    static STUFFING_POTATO = "STUFFING_POTATO";
-    static TOPPING_MAYO = "TOPPING_MAYO";
-    static TOPPING_SPICE = "TOPPING_SPICE";
 
     get size(){
         return this._size;
@@ -64,10 +57,28 @@ class Hamburger{
         Hamburger.checkData.call(this,stuffs,val,'_stuffing');
     }
 
+    get topping(){
+        return this.getToppings()
+    }
+
+    set topping(val){/*can't be changed*/}
+
+    static SIZE_SMALL = "SIZE_SMALL";
+    static SIZE_LARGE = "SIZE_LARGE";
+    static STUFFING_CHEESE = 'STUFFING_CHEESE';
+    static STUFFING_SALAD = "STUFFING_SALAD";
+    static STUFFING_POTATO = "STUFFING_POTATO";
+    static TOPPING_MAYO = "TOPPING_MAYO";
+    static TOPPING_SPICE = "TOPPING_SPICE";
+
+    static countAmount (field){
+        return this._size[field] + this._stuffing[field] + (this._topping[0] ? (this._topping.length===1?this._topping[0][field]:(this._topping.reduce((sum, element)=>{return sum[field] + element[field]}))): 0);
+    };
+
     static checkData (array,field,nameVariable){
         try {
             if(field) {
-                array.some((element) => {
+                if(!array.some((element) => {
                     if (element.name === field) {
                         Object.defineProperty(this, nameVariable, {
                             value: element,
@@ -75,16 +86,91 @@ class Hamburger{
                             writable: false
                         });
                         return true;
-                    } else {
-                        throw new HamburgerException(`invalid ${nameVariable} '${field}'`);
                     }
-                });
+                })){
+                    throw new HamburgerException(`invalid ${nameVariable} '${field}'`);
+                }
             }else {
                 throw new HamburgerException(`no given ${nameVariable}`);
             }
         }catch (e) {
             console.error(e.message);
         }
+    };
+
+    addTopping (_topping) {
+
+        try {
+
+            //проверка на наличие переданого значения
+            if(_topping){
+                //проверка с колекцией данных, существует ли такой объект
+                if (!toppings.some((element)=>{
+                    if(element.name===_topping){
+                        //проверка на дубликат
+                        if(!this._topping.some((element)=>{return element.name === _topping;})){
+                            this._topping.push(element);
+                        }
+                        else {
+                            throw new HamburgerException(`duplicate topping '${_topping}'`);
+                        }
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                })) {
+                    throw new HamburgerException(`invalid ${_topping}`);
+                }
+            }else {
+                throw new HamburgerException('no given topping');
+            }
+        }catch (e) {
+            console.error(e.message);
+        }
+    };
+
+    removeTopping (_topping) {
+        try {
+            //проверка на переданные данные
+            if(_topping){
+                //проверка на существование таких данных в текущих топпингах
+                if(!this._topping.some((element)=>{
+                    if(element.name===_topping) {
+                        this._topping = this._topping.filter((element)=>{return element.name!==_topping});
+                        return true
+                    }
+                })){
+                    throw new HamburgerException(`${_topping} is not defined`);
+                }
+            }else {
+                throw new HamburgerException('no given topping');
+            }
+        }catch (e) {
+            console.error(e.message);
+        }
+    };
+
+    getToppings () {
+        return this._topping.map((element)=>{
+            return element.name;
+        });
+    };
+
+    getSize () {
+        return this._size.name;
+    };
+
+    getStuffing () {
+        return this._stuffing.name;
+    };
+
+    calculatePrice () {
+        return  Hamburger.countAmount.call(this,"price")
+    };
+
+    calculateCalories () {
+        return  Hamburger.countAmount.call(this,"calories")
     };
 
 }
@@ -100,5 +186,37 @@ function HamburgerException (message){
 }
 
 
-debugger;
-let hamburger = new Hamburger(Hamburger.SIZE_LARGE, Hamburger.STUFFING_POTATO);
+
+console.log('большой гамбургер с начинкой из салата');
+let hamburger = new Hamburger(Hamburger.SIZE_LARGE, Hamburger.STUFFING_SALAD);
+console.log('добавка из майонеза');
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
+console.log('спросим сколько там калорий');
+console.log("Calories: %f", hamburger.calculateCalories());
+console.log('сколько стоит');
+console.log("Price: %f", hamburger.calculatePrice());
+console.log('я тут передумал и решил добавить еще приправу');
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
+console.log('А сколько теперь стоит?');
+console.log("Price with sauce: %f", hamburger.calculatePrice());
+console.log('Проверить, большой ли гамбургер?');
+console.log("Is hamburger large: %s", hamburger.getSize() === Hamburger.SIZE_LARGE);
+console.log('Убрать добавку');
+hamburger.removeTopping(Hamburger.TOPPING_SPICE);
+console.log("Have %d toppings", hamburger.getToppings().length);
+console.log('узнать начинку');
+console.log(hamburger.getStuffing());
+
+
+// // не передали обязательные параметры
+// let h2 = new Hamburger(); // => HamburgerException: no size given
+//
+// передаем некорректные значения, добавку вместо размера
+// let h3 = new Hamburger(Hamburger.TOPPING_SPICE, Hamburger.TOPPING_SPICE);
+// => HamburgerException: invalid size 'TOPPING_SAUCE'
+//
+// // добавляем много добавок
+// let h4 = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+// h4.addTopping(Hamburger.TOPPING_MAYO);
+// h4.addTopping(Hamburger.TOPPING_MAYO);
+// // HamburgerException: duplicate topping 'TOPPING_MAYO'
